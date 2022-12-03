b512e22c074a83e121867aa1f37acdda45182ce3
kombuchan <76601670+mihikakrishna@users.noreply.github.com>
2022-12-02 20:48:14 -0800

    Day 2 - Parts 1 and 2 Solved

---
+++ day2/part1_solution.py
@@ -0,0 +1,33 @@
+def RockPaperScissors(part1InputFile):
+    f = open(part1InputFile, 'r')
+    input = [line.rstrip('\n') for line in f]
+
+    shapesDict = {
+        "A" : 1,
+        "B" : 2,
+        "C" : 3,
+        "X" : 1,
+        "Y" : 2,
+        "Z" : 3
+    }
+
+    my_score = 0
+
+    for line in input:
+        line = line.split(' ')
+        opp_play = shapesDict[line[0]]
+        my_play = shapesDict[line[1]]
+
+        if opp_play == my_play:
+            my_score += 3         
+
+        elif ((my_play == 1 and opp_play == 3)
+            or (my_play == 2 and opp_play == 1)
+            or (my_play == 3 and opp_play == 2)):
+            my_score += 6
+
+        my_score += my_play
+        
+    print(my_score)
+
+RockPaperScissors('day2\part1_input.txt')
---
+++ day2/part2_solution.py
@@ -0,0 +1,40 @@
+def RockPaperScissors(part1InputFile):
+    f = open(part1InputFile, 'r')
+    input = [line.rstrip('\n') for line in f]
+
+    shapesDict = {
+        "A" : {
+            "X" : 3,
+            "Y" : 1,
+            "Z" : 2
+        },
+        "B" : {
+            "X" : 1,
+            "Y" : 2,
+            "Z" : 3
+        },
+        "C" : {
+            "X" : 2,
+            "Y" : 3,
+            "Z" : 1
+        }
+    }
+
+    outcomeDict = {
+        "X" : 0,
+        "Y" : 3,
+        "Z" : 6
+    }
+
+    my_score = 0
+
+    for line in input:
+        line = line.split(' ')
+        opp_play = line[0]
+        outcome = line[1]
+
+        my_score += shapesDict[opp_play][outcome] + outcomeDict[outcome]
+
+    print(my_score)
+
+RockPaperScissors('day2\part2_input.txt')
